22597a687e885289b0cb31e6b844a3c08b4e0492
okmaheer <86462580+okmaheer@users.noreply.github.com>
2022-06-24 01:48:58 +0500

    registeration, or iscoaches  add

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -1,4 +1,17 @@
 export default {
  
+    async registerCoach(context, data) {
+    
+      const coachData = {
+          id: context.rootGetters.userId,
+        firstName: data.first,
+        lastName: data.last,
+        description: data.desc,
+        hourlyRate: data.rate,
+        areas: data.areas
+      };
   
+  
+      context.commit('registerCoach', {coachData});
+    },
   };
--- src/store/modules/coaches/mutations.js
+++ src/store/modules/coaches/mutations.js
@@ -1,4 +1,8 @@
 export default {
- 
+    registerCoach(state, payload) {
+        console.log(payload);
+        state.coaches.push(payload.coachData);
+    
+      },
   
 };
