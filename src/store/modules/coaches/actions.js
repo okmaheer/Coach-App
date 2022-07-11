48f5915e94ecdf7f7d41b862b8e51efcea5d31dc
okmaheer <86462580+okmaheer@users.noreply.github.com>
2022-07-11 13:51:32 +0500

    changes

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,10 +9,13 @@ export default {
       areas: data.areas
     };
 
-    const response = await fetch(`https://vue-http-demo-85e9e.firebaseio.com/coaches/${userId}.json`, {
-      method: 'PUT',
-      body: JSON.stringify(coachData)
-    });
+    const response = await fetch(
+      `https://coachesapp-38df0-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      {
+        method: 'PUT',
+        body: JSON.stringify(coachData)
+      }
+    );
 
     // const responseData = await response.json();
 
@@ -24,5 +27,37 @@ export default {
       ...coachData,
       id: userId
     });
+  },
+  async loadCoaches(context, payload) {
+    if (!payload.forceRefresh && !context.getters.shouldUpdate) {
+      return;
+    }
+
+    const response = await fetch(
+      `https://coachesapp-38df0-default-rtdb.firebaseio.com/coaches.json`
+    );
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      const error = new Error(responseData.message || 'Failed to fetch!');
+      throw error;
+    }
+
+    const coaches = [];
+
+    for (const key in responseData) {
+      const coach = {
+        id: key,
+        firstName: responseData[key].firstName,
+        lastName: responseData[key].lastName,
+        description: responseData[key].description,
+        hourlyRate: responseData[key].hourlyRate,
+        areas: responseData[key].areas
+      };
+      coaches.push(coach);
+    }
+
+    context.commit('setCoaches', coaches);
+    context.commit('setFetchTimestamp');
   }
 };
